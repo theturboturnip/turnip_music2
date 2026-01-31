0451c6c0e3fe9955e01d1645d841eaa9ba400ac7
Samuel Stark <samuel.winslow.stark@gmail.com>
2026-01-31 16:22:17 +0000

    Data model

---
+++ src/data_model.rs
@@ -0,0 +1,226 @@
+//! turnip_music2 operates on input files to build output libraries.
+//! Input files come in a few flavors:
+//! - Config files [user_defined::ConfigFile], that aren't associated with specific music files but affect global behaviour.
+//!   These are passed in as top-level command line arguments and are usually named `library.tm2.toml`.
+//!   Examples of controls are global renamings for artists.
+//! - Group Metadata [user_defined::CompilationInputGroup] [user_defined::AlbumInputGroup] , stored in `music.tm2.toml` files in folders containing source music files.
+//!   These control the metadata for those source music files, including information on where they came from,
+//!   which affects how those files are then transcoded and output.
+//!
+//!   For example, it holds the `Origin` data on where the group came from (e.g. if it was ripped from a disc, which disc?);
+//!   and any media-specific overrides for that metadata.
+//!   A separate file (TODO: TOML or SQLite?) also holds:
+//!    - a cache of the derived metadata source, found automatically from the Origin;
+//!    - a cache of the actual metadata extracted from that source for each song;
+//! - Source Music files, stored inside folders (recursive search) containing Group Metadata files.
+//! 
+//! Loading a library consists of 
+//! - Gathering all the Groups you can find
+//! - Within those Groups, scanning for relevant Songs
+//! - Searching for any missing metadata
+//! - Resolving the metadata for each Song
+//!     - Start with the metadata encoded within the source song
+//!     - If there is cached metadata from Musicbrainz, override with that
+//!         - If the Song is inside an Album Group, the metadata for the Song is derived from that of the Album's MusicBrainz release
+//!           *and* the media index/track index of the Song.
+//!             - the "source" disc and track indices of each Song are derived from the source file metadata if present, and otherwise
+//!           are respectively kept constant and incremented from the previous Song in an alphanumeric sorting by file name within the Group,
+//!           starting at (1,1).
+//!             - TODO the Album Group should then have the ability to offset the track number or fix the disc number
+//!             - The song metadata is then looked up from the given media and the given track.
+//!             - If the track number is too large for the given media index, increment the media index and decrement the track number by the length of that media.
+//!             - This allows long sequential incrementing track numbers to be automatically split across disks.
+//!         - If the Song is inside a Compilation Group, the metadata for the song is derived from the origin MusicBrainz ID if one is present.
+//!     - If there is override metadata in the Group Metadata file, override with that
+//! - Creating a 1:1 mapping of Songs -> output Songs
+//!     - if within an Album Group, `<First Artist of Album>/<Album Name>/<Song Name>`
+//!     - if within a Compilation Group, `<First Artist of Song>/<Song Name>`
+//!     - all path components are deduplicated if necessary with uppercase alpha "ABCDE..." postfixes.
+//!     - if any path component contains special characters the output process stops (UTF-8 allowed, but not filesystem-breakers such as NTFS `/\:*"?<>|`)
+//! - Use FFMPEG to render out output files
+//!     - If same extension, don't bother - avoid recompressing MP3->MP3? TODO add config option for that
+//!     - If same input file hash as previous (job cache?) and output file exists
+//!         - TODO if output file has different hash than expected, also rerender?
+//!         - if input and output file hashes change that indicates loss of integrity, if input file is the same assume that's fine?
+//!     - Delete output files that aren't supposed to be there.
+//! - Create .m3u8 files for the compilations
+//!     - Can just delete old ones and remake, no point in doing sensitivity there?
+//!     - Compilations retain the same track ordering as alphanumeric input file sorting, so ordered compilations can be created if desired but otherwise do not matter.
+
+/// MusicBrainz ID <https://musicbrainz.org/doc/MusicBrainz_Identifier>,
+/// which can be for one of many different kinds of [entities](https://musicbrainz.org/doc/MusicBrainz_Entity)
+struct MbId(String);
+/// https://musicbrainz.org/doc/Disc_ID
+struct MbDiscId(String);
+/// https://en.wikipedia.org/wiki/CDDB#Example_calculation_of_a_CDDB1_(FreeDB)_disc_ID
+struct CddbDiscId(String);
+
+/// Data types defining the user-controlled TOML files
+pub mod user_defined {
+    use crate::data_model::{CddbDiscId, MbDiscId, MbId, metadata};
+
+    pub struct ConfigFile {
+        pub search_paths: Vec<String>,
+        pub artist_name_overrides: Vec<ConfigArtistNameOverride>,
+    }
+
+    pub struct ConfigArtistNameOverride {
+        pub artist_id: MbId,
+        pub artist_name: String,
+    }
+    
+    /// A set of concrete sources for metadata, controlled by the user, that are never discarded.
+    pub struct Origin {
+        pub url: Option<String>,
+        pub mb_release_group_id: Option<MbId>,
+        pub mb_release_id: Option<MbId>,
+        pub mb_discid: Option<MbDiscId>,
+        pub cddb_discid: Option<CddbDiscId>,
+    }
+
+    /// A filter for the files to actually scan and use ---
+    /// in case of icky input directories with different copies of the same music
+    pub struct ScanFilter {
+        /// e.g. \['mp3', 'flac'\]
+        pub ext_filters: Vec<String>,
+    }
+    
+    pub enum GroupFile {
+        Compilation(CompilationInputGroup),
+        Album(AlbumInputGroup),
+    }
+    impl GroupFile {
+        pub fn scan_filter(&self) -> Option<&ScanFilter> {
+            match self {
+                GroupFile::Compilation(input) => input.scan_filter.as_ref(),
+                GroupFile::Album(input) => input.scan_filter.as_ref(),
+            }
+        }
+    }
+
+    pub struct CompilationInputGroup {
+        pub origin: Origin,
+        pub scan_filter: Option<ScanFilter>,
+        pub title: String,
+        pub songs: Vec<CompilationInputSongOverride>,
+    }
+
+    pub struct CompilationInputSongOverride {
+        pub file_rel_path: String,
+        pub origin_mbid: Option<MbId>,
+        pub override_metadata: Option<metadata::song::Override>,
+        pub override_position: Option<u64>,
+    }
+
+    pub struct AlbumInputGroup {
+        pub origin: Origin,
+        pub scan_filter: Option<ScanFilter>,
+        pub album_art_rel_path: Option<String>,
+        pub override_metadata: Option<metadata::album::Override>,
+        pub songs: Vec<AlbumInputSongOverride>,
+    }
+
+    pub struct AlbumInputSongOverride {
+        pub file_rel_path: String,
+        pub override_metadata: Option<metadata::song::Override>,
+        pub override_disc_idx: Option<u64>,
+        pub override_track_idx: Option<u64>,
+    }
+}
+
+/// Data types for metadata, both cached and overridden by users.
+pub mod metadata {
+    use super::*;
+
+    pub struct CachedArtist {
+        id: MbId,
+        name: String,
+    }
+
+    pub mod song {
+        use super::CachedArtist;
+
+        pub struct Override {
+            song_title: Option<String>,
+            song_artists: Option<Vec<String>>,
+        }
+
+        pub struct Cached {
+            song_title: String,
+            song_artists: Vec<CachedArtist>,
+        }
+
+        pub struct Output {
+            song_title: String,
+            song_artists: Vec<String>,
+        }
+    }
+    pub mod album {
+        use crate::data_model::MbId;
+        use super::CachedArtist;
+
+        pub struct DerivedMetadataSource {
+            mb_release_group_id: Option<MbId>,
+            mb_release_id: Option<MbId>,
+            /// for e.g. a single disc in a larger album
+            mb_release_media_idx: Option<u64>,
+        }
+
+        pub struct Override {
+            album_title: Option<String>,
+            album_artists: Option<Vec<String>>,
+        }
+
+        pub struct Cached {
+            title: String,
+            artists: Vec<CachedArtist>,
+        }
+    }
+}
+
+struct FileId {
+    /// '/' coded path relative to the library config TOML file being read, NOT to the group TOML file.
+    path: String,
+    /// Base64 encoded SHA256 digest of the file, used for integrity checks
+    hash: String,
+}
+
+pub struct CompilationInputGroup {
+    origin: user_defined::Origin,
+    scan_filter: Option<user_defined::ScanFilter>,
+    title: String,
+    song_files: Vec<CompilationInputSong>,
+}
+
+pub struct CompilationInputSong {
+    file: FileId,
+    origin_mbid: Option<MbId>,
+    override_metadata: Option<metadata::song::Override>,
+
+    cached_metadata: Option<metadata::song::Cached>,
+}
+
+pub struct AlbumInputGroup {
+    origin: user_defined::Origin,
+    override_metadata: Option<metadata::album::Override>,
+    scan_filter: Option<user_defined::ScanFilter>,
+    album_art: FileId,
+
+    song_files: Vec<AlbumInputSong>,
+
+    derived_metadata_src: Option<metadata::album::DerivedMetadataSource>,
+    cached_metadata: Option<metadata::album::Cached>,
+}
+
+pub struct AlbumInputSong {
+    file: FileId,
+    override_metadata: Option<metadata::song::Override>,
+    disc_idx: u64,
+    track_idx: u64,
+
+    derived_mbid: Option<MbId>,
+    cached_metadata: Option<metadata::song::Cached>,
+}
+
+
+
---
+++ src/lib.rs
@@ -0,0 +1,107 @@
+use std::collections::HashSet;
+use std::ffi::{OsStr, OsString};
+use std::path::PathBuf;
+use crate::data_model::{user_defined, AlbumInputGroup, CompilationInputGroup};
+
+mod data_model;
+
+const GROUP_FILE_NAME: &'static str = "music.tm2.toml";
+const DEFAULT_MUSIC_EXTS: [&'static str; 6] = [
+    "mp3",
+    "ogg",
+    "flac",
+    "wav",
+    "aiff",
+    "m4a",
+    // TODO m4b support one day? requires general splitting-big-file support.
+];
+
+// TODO need input file metadata parsing using rust-metaflac for FLAC and mp4ameta for m4as and id3 for others
+// see docs for each crate
+
+struct LibraryContext {
+    root_path: PathBuf,
+    config: user_defined::ConfigFile,
+
+    cache: ,
+
+    album_groups: Vec<AlbumInputGroup>,
+    compilation_groups: Vec<CompilationInputGroup>,
+}
+
+impl LibraryContext {
+    fn scan_library(&mut self) -> anyhow::Result<()> {
+        let mut scan_stack = vec![self.root_path];
+        let group_file_name = OsStr::new(GROUP_FILE_NAME);
+
+        while let Some(dir) = scan_stack.pop() {
+            let mut files = vec![];
+            let mut dirs = vec![];
+            let mut group = None;
+
+            for entry in std::fs::read_dir(&dir)? {
+                let entry = entry?;
+                let path = entry.path();
+
+                if path.is_dir() {
+                    dirs.push(path);
+                } else if path.is_file() {
+                    if path.file_name() == Some(group_file_name) {
+                        group = Some(user_defined::GroupFile::from_file(path)?);
+                    } else {
+                        files.push(path);
+                    }
+                }
+            }
+
+            if let Some(group) = group {
+                self.scan_group(group, dirs, files)?;
+            } else {
+                scan_stack.extend(dirs);
+            }
+        }
+
+        Ok(())
+    }
+
+    fn scan_group(&mut self, group: user_defined::GroupFile, root_dirs: Vec<PathBuf>, root_files: Vec<PathBuf>) -> anyhow::Result<()> {
+        let mut scan_stack = root_dirs;
+        // TODO have to include path-relative-to-root_dirs
+        let mut music_files: Vec<PathBuf> = vec![];
+        let scan_exts: HashSet<OsString> = group.scan_filter().map_or_else(
+            || DEFAULT_MUSIC_EXTS.iter().map(|s| s.into()).collect(),
+            |scan_filter| scan_filter.ext_filters.iter().map(|s| s.into()).collect()
+        );
+
+        for path in root_files {
+            if let Some(ext) = path.extension() {
+                if scan_exts.contains(ext) {
+                    music_files.push(path);
+                }
+            }
+        }
+
+        while let Some(dir) = scan_stack.pop() {
+            for entry in std::fs::read_dir(&dir)? {
+                let entry = entry?;
+                let path = entry.path();
+
+                if path.is_dir() {
+                    scan_stack.push(path);
+                } else if path.is_file() {
+                    if let Some(ext) = path.extension() {
+                        if scan_exts.contains(ext) {
+                            music_files.push(path);
+                        }
+                    }
+                }
+            }
+
+        }
+
+        todo!("Actually process these as per data_model documentation. Don't call out to MusicBrainz yet, save that for later");
+
+        Ok(())
+    }
+}
+// Sta
